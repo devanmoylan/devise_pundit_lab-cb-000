001590f4ab3157e888405a70dc981c35d2edcf88
Devan <devan.moylan@backyardlion.com>
2017-08-10 11:42:29 -0600

    Add role to users. Add policies to user policies.

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,2 +1,23 @@
 class UserPolicy < ApplicationPolicy
+
+  def index?
+    user_admin?
+  end
+
+  def destroy?
+    user_admin?
+  end
+
+  def show?
+    user_admin?
+  end
+
+  def update?
+    user_admin?
+  end
+
+  def user_admin?
+    user.admin?
+  end
+
 end
